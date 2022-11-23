52ce0d2cf9bb59bd610c064e10818e97bfd51dd1
yuvrajanalyst <uvrushty@gmail.com>
2022-11-23 11:07:31 +0530

    while loop guess game

--- guessing game2.0.py
+++ guessing game2.0.py
@@ -1,2 +1,23 @@
-x = 5 + 5
-print(x)
+# Create a gussing game using while loop
+
+# Impoer random number generator 
+import random
+
+generator = random.randint(1,10)
+
+guess = int(input('Guess a right number: '))
+attemp = 1
+
+while guess != generator:
+    if guess < generator:
+        print('Try higher number again!')
+    else:
+        print('Try Lower value! ')
+    guess = int(input('Guess a right number: '))
+    attemp += 1
+
+else:
+    print('Correct.')
+    print('attemp:', attemp)
+
+print('jackpot: ',generator)
