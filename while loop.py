5c256f1635381f8c9a1ef7dd73b0cfabb219d5e5
yuvrajanalyst <uvrushty@gmail.com>
2022-11-23 15:30:32 +0530

    while anf for loop

--- while loop.py
+++ while loop.py
@@ -1,3 +1,23 @@
-y=5
-x=2
-print(y+x)
+# 01 > create a program count 10 to 100 sum of 10:
+sum10 = 10
+while sum10 <= 100:
+    print(sum10)
+    sum10 += 10
+
+# 02 >
+'''
+The current population town is 10000. The population of the town is 
+increasing at the rate if 10% per year. Ypu have to write a program to find out the 
+population at the end of each of the last 10 year.
+'''
+now_days = 10000
+for i in range(10,0,-1):
+    print(round(now_days),0)
+    now_days = now_days - 0.1*now_days
+
+
+current_pop = 10000
+for i in range(0,11):
+    current_pop=current_pop+0.1*current_pop
+    print(round(current_pop),0)
+
